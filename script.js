391f37fe4e360c4446dcb15218a38378c0ab9656
Heba ElGalad <heba957@gmail.com>
2020-04-10 00:55:12 +0200

    Add form validations & submit sign up form

--- script.js
+++ script.js
@@ -2,6 +2,92 @@ var API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
 var API_PATH_SIGNUP = "/auth/register";
 var API_PATH_SIGNIN = "/auth/login";
 
+//  Get Sign-up input fields
+const nameField = document.getElementById("name");
+const usernameField = document.getElementById("username");
+const passwordField = document.getElementById("password");
+const emailField = document.getElementById("email");
+
+// email validation regex
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+// Validate Form Fields function
+function formValidation(fieldName) {
+  if (!fieldName.value) {
+    fieldName.nextElementSibling.style.display = "inline-block";
+  } else if (fieldName.value) {
+    fieldName.nextElementSibling.style.display = "none";
+  }
+}
+
+// Validate Email Field function
+function emailValidation(fieldName) {
+  if (!emailField.value || !emailField.value.match(emailRegex)) {
+    emailField.nextElementSibling.style.display = "inline-block";
+  } else if (emailField.value.match(emailRegex)) {
+    emailField.nextElementSibling.style.display = "none";
+  }
+}
+
+// SUBMIT SIGN-UP FORM
+$("#sign-up-form").submit(function (event) {
+  // Prevent page refreshes on submit
+  event.preventDefault();
+
+  // 1.Validate the Fields before submit
+  formValidation(nameField);
+  formValidation(usernameField);
+  formValidation(passwordField);
+  emailValidation(emailField);
+
+  if (
+    nameField.value &&
+    usernameField.value &&
+    passwordField.value &&
+    emailField.value
+  ) {
+    // 2.Display loader
+    $(".loader-container").css("display", "flex");
+
+    // 3.Get the form data as array and stringify it.
+    let formData = $(this).serializeArray();
+
+    let jsonObject = {};
+    for (let i = 0; i < formData.length; i++) {
+      let key = formData[i].name;
+      jsonObject[key] = formData[i].value;
+    }
+
+    // 4.Setting the AJAX request
+    $.ajax({
+      type: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      url: `${API_URL}${API_PATH_SIGNUP}`,
+      data: JSON.stringify(jsonObject),
+      success: function (res) {
+        $(".loader-container").css("display", "none");
+        $(".form-alerts").css("display", "flex");
+        $(".form-alerts").css("background-color", "#00b25b");
+        $(".form-alerts span").text(`${res.responseJSON.message}`);
+      },
+      error: function (err) {
+        $(".loader-container").css("display", "none");
+        $(".form-alerts").css("display", "flex");
+        $(".form-alerts").css("background-color", "#dc3545");
+        $(".form-alerts span").text(`${err.responseJSON.message}`);
+      },
+      dataType: "json",
+    });
+  }
+});
+
+// Close form alerts container
+$(".close-button").click(function () {
+  $(".form-alerts").css("display", "none");
+});
+
 // Toggle Sign in & Sign up Tabs
 $(".sign-up-tab").click(function () {
   $("#sign-up-form").css("display", "block");
